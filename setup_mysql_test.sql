242ca92f73bba1ebcd93b0cd9254c2cc6fc6c93a
IshimweJ-Ona <j.ishimwe4@alustudent.com>
2026-02-02 17:51:37 +0200

    Updates with all tables

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -5,7 +5,7 @@
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
 -- Create the test user if it doesn't exist and set password
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED WITH mysql_native_password BY 'hbnb_test_pwd';
 
 -- Grant all privileges on hbnb_test_db to hbnb_test
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
