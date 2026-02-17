9cc288d81d89f9661bbab3cb78b35f066fc39a87
IshimweJ-Ona <j.ishimwe4@alustudent.com>
2026-02-17 18:01:58 +0200

    Script to prepare servers for deployment

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+# Setup web server for deployment of web_static
+
+# We install nginx if not already installed
+if ! dpkg -s nginx >/dev/null 2>&1; then
+     sudo apt update -y
+     sudo apt install -y nginx
+fi
+
+# Create required directories
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+
+# Create fake html file
+echo "<html>
+  <head></head>
+  <body>
+    Webstatic deployment
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+
+# Remove existing symbolic link if it exists
+sudo rm -rf /data/web_static/current
+
+# create new symbolic link
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+
+# Set ownership to ubuntu user and group (recursive)
+sudo chown -R ubuntu:ubuntu /data/
+
+# Update nginx configuration to serve hbnb_static
+NGINX_CONF="/etc/nginx/sites-available/default"
+
+if ! grep -q "hbnb_static" "$NGINX_CONF"; then
+    sudo sed -i "/server_name _;/a \
+    location /hbnb_static/ {\n\
+        alias /data/web_static/current/;\n\
+    }" "$NGINX_CONF"
+fi
+
+# Restart nginx
+sudo service nginx restart
+
+# Always exit successfully
+exit 0
+
